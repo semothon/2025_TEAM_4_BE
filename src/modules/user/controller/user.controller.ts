375af6d51e319493f2633acf93480e0bed4c6929
cylin0201 <cylin0201@naver.com>
2025-04-04 16:50:14 +0900

    test over

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -17,6 +17,7 @@ export class UserController {
   @Post('/sign-in')
   @ApiOperation({ summary: '로그인', description: '사용자 로그인' })
   async signIn(@Body() signInDto: SignInDto) {
+    //console.log("로그인 성공!");
     return this.userService.signIn(signInDto);
   }
 }
