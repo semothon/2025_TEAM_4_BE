498e341efb0b451a3c520e3c9b70107ffc5156dd
KHJcode <to@khjcode.com>
2025-04-06 06:36:49 +0900

    update

--- src/modules/user-pattern/controller/user-pattern.controller.ts
+++ src/modules/user-pattern/controller/user-pattern.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Get, Param, Post, Put, UseGuards} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
+import { UserPattern } from '@prisma/client';
 import { AccessGuard } from '../../common/security/access.guard';
 import { USER_PERSONALITY_TYPE } from '../../user-type-score/constants/user-personality-type';
 import { CreateUserPatternDto, UpdateUserPatternDto } from '../model/user-pattern.dto';
 import { UserPatternService } from '../service/user-pattern.service';
-import { UserPattern } from '@prisma/client';
 
 @ApiTags('UserPattern')
 @Controller('user-pattern')
 export class UserPatternController {
-  constructor(private readonly userPatternService: UserPatternService) {}
+  public constructor(private readonly userPatternService: UserPatternService) {}
 
   @Get('personality-types')
   @ApiResponse({ status: 200, description: '사용자 성격 분류 유형 리스트 반환' })
@@ -21,7 +21,7 @@ export class UserPatternController {
   @Post()
   @ApiOperation({ summary: '사용자 패턴 정보 생성', description: '개인의 생활 패턴 정보를 등록합니다.' })
   @ApiResponse({ status: 201, description: '사용자 패턴 등록 성공', type: CreateUserPatternDto })
-  public create(@Body() createUserPatternDto: CreateUserPatternDto): Promise<UserPattern> {
+  public async create(@Body() createUserPatternDto: CreateUserPatternDto): Promise<UserPattern> {
     return this.userPatternService.createUserPattern(createUserPatternDto);
   }
 
@@ -30,7 +30,7 @@ export class UserPatternController {
   @ApiOperation({ summary: '사용자 패턴 정보 수정', description: '사용자의 패턴 정보를 수정합니다.' })
   @ApiParam({ name: 'userId', type: Number })
   @ApiResponse({ status: 200, description: '패턴 정보 수정 완료', type: UpdateUserPatternDto })
-  public update(
+  public async update(
     @Param('userId') userId: number,
     @Body() updateUserPatternDto: UpdateUserPatternDto,
   ): Promise<UserPattern> {
